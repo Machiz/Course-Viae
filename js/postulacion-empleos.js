ec10844838f994f68b1912fcb36a7e34b0f3d64a
Moroff3 <Crostzard@outlook.com>
2025-06-09 11:20:32 -0500

    feat: Se agregó un aviso visual en los bordes de los formularios en caso estén vacíos.

--- js/postulacion-empleos.js
+++ js/postulacion-empleos.js
@@ -5,10 +5,24 @@ const pos_but = document.getElementById('postular1');
 const main_box = document.getElementById('popup1');
 const bg = document.getElementById('bg');
 console.log("hola");
+const texto1 = document.getElementById("in-1");
+const texto2 = document.getElementById("in-2");
+texto1.addEventListener('blur', (e) => {
+if(e.target.value.trim().length == 0){
+    e.target.classList.add('warning')
+}else{
+    e.target.classList.remove('warning')
+}})
+
+texto2.addEventListener('blur', (e) => {
+if(e.target.value.trim().length == 0){
+    e.target.classList.add('warning')
+}else{
+    e.target.classList.remove('warning')
+}})
 function culminar_postulacion(){
     
-    const texto1 = document.getElementById("in-1");
-    const texto2 = document.getElementById("in-2");
+    
 
     if (texto1.value.trim() !== "" && texto2.value.trim() !== "" ) {
         bg.classList.add("dark-bg-show");
